3d30496a7a3166aa8cddb1117c30334fefde5dfb
Amit Sah <amitsah04v@gmail.com>
2026-03-01 18:36:36 +0530

    B Deletion sort

--- B_Deletion_Sort.js
+++ B_Deletion_Sort.js
@@ -1,30 +1,22 @@
-// var input = require("fs").readFileSync(0, "utf8").trim().split(/\s+/);
-// var testCases = input[0];
-// let ind = 1;
+var input = require("fs").readFileSync(0, "utf8").trim().split(/\s+/);
+var testCases = input[0];
+let ind = 1;
 
-// for (let i = 0; i < testCases; i++) {
-//   let n = Number(input[ind++]);
-//   let arr = [];
+for (let i = 0; i < testCases; i++) {
+  let n = Number(input[ind++]);
+  let arr = [];
 
-//   for (let i = 0; i < n; i++) {
-//     arr.push(Number(input[ind++]));
-//   }
+  for (let i = 0; i < n; i++) {
+    arr.push(Number(input[ind++]));
+  }
+  console.log(DeletionSort(n, arr));
+}
 
-//   console.log(DeletionSort(n, arr));
-// }
-
-// function DeletionSort(n, nums) {
-//   // for non-decreasing arrays
-//   let numsCopy = [...nums];
-//   numsCopy.sort((a, b) => a - b);
-
-//   let i = 0;
-//   while (i < nums.length) {
-//     if (numsCopy[i] !== nums[i]) {
-//       break;
-//     }
-//     i++;
-//   }
-//   if (i === numsCopy.length) return numsCopy.length;
-  
-// }
+function DeletionSort(n, nums) {
+  for (let i = 1; i < n; i++) {
+    if (nums[i] < nums[i - 1]) {
+      return 1;
+    }
+  }
+  return n;
+}
