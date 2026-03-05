0dec3c7fe6d566ece079c43628dc34ede2a86d0d
Amit Sah <amitsah04v@gmail.com>
2026-03-05 23:34:11 +0530

    wip

---
+++ A_Round_Down_the_Price.js
@@ -0,0 +1,28 @@
+// var input = require("fs")
+//   .readFileSync(0, "utf8")
+//   .trim()
+//   .split(/\s+/)
+//   .map(Number);
+// var testCases = input[0];
+// var inp = 1;
+
+// for (let i = 0; i < testCases; i++) {
+//   let n = input[inp++];
+//   if (n === 1) {
+//     console.log(0);
+//     continue;
+//   }
+//   if (n < 10) {
+//     console.log(n - 1);
+//     continue;
+//   }
+
+//   let copy = n;
+//   let tens = 1;
+
+//   while (copy > 1) {
+//     tens = tens * 10;
+//     copy = copy / 10;
+//   }
+//   console.log(n - tens / 10);
+// }
