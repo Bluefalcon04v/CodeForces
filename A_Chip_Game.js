225c3e6cff6fcea8e4b95be30569935b5de5ccf3
Amit Sah <amitsah04v@gmail.com>
2026-03-27 12:14:42 +0530

    A Chip Game

--- A_Chip_Game.js
+++ A_Chip_Game.js
@@ -6,17 +6,13 @@ var input = require("fs")
 
 let test_cases = input[0];
 let index = 1;
+
 for (let i = 0; i < test_cases; i++) {
   let n = input[index++];
   let m = input[index++];
 
-  if (n === 1 && m == 1) {
+  if ((n + m) % 2 === 0) {
     console.log("Tonya");
-    continue;
-  }
-  if (n % 2 === 0 && m % 2 === 0) {
-    console.log("Tonya");
-    continue;
   } else {
     console.log("Burenka");
   }
