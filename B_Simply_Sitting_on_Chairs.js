4afb356450721d135bc7017a11621e54dc2fe007
Amit Sah <amitsah04v@gmail.com>
2026-03-30 12:52:04 +0530

    B Simply Sitting on Chairs

--- B_Simply_Sitting_on_Chairs.js
+++ B_Simply_Sitting_on_Chairs.js
@@ -6,22 +6,15 @@ let input = require("fs")
 
 let t = input[0];
 let index = 1;
-let output = [];
 
 while (t--) {
   const n = input[index++];
   const p = Array.from({ length: n }, () => input[index++]);
 
-  const marked = Array(n + 1).fill(false);
   let chairsSat = 0;
-
-  for (let i = 1; i <= n; i++) {
-    if (marked[i]) break;
-    chairsSat++;
-    marked[p[i - 1]] = true;
+  for (let i = 0; i < n; i++) {
+    if (p[i] <= i + 1) chairsSat++;
   }
 
-  output.push(chairsSat);
+  console.log(chairsSat);
 }
-
-console.log(output.join("\n"));
